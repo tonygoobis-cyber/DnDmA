034dd0499cb1b5d85a5b631a292b596574a205db
Tony Goobis <tonygoobis@gmail.com>
2026-03-26 14:25:30 -0400

    Update offsets in Offsets.cs TEST
    This is just test run of these offsets. Also we dont know if the cheese itself works.

--- Source/Misc/Offsets.cs
+++ Source/Misc/Offsets.cs
@@ -11,49 +11,49 @@ namespace DMAW_DND
 {
     internal static class Offsets
     {
-        public const int GWorld = 0x9D1EC18; //updated
-        public const int GNames = 0x9B07A00; //updated
-        public const int GameStateBase = 0x158; //Engine.World
-        public const int PlayerArray = 0x2B0; //Engine.GameStateBase
-        public const int LevelsArray = 0x170;  //Engine.World
-        public const int ActorsArray = 0x98; //Engine.Level
-        public const int OwningGameInstance = 0x1B8; //Engine.World
+        public const int GWorld = 0xc53c440; //updated 
+        public const int GNames = 0xc2da2c0; //updated
+        public const int GameStateBase = 0x160; //Engine.World
+        public const int PlayerArray = 0x2c0; //Engine.GameStateBase
+        public const int LevelsArray = 0x178;  //Engine.World
+        public const int ActorsArray = 0x28; //Engine.Level
+        public const int OwningGameInstance = 0x1d8; //Engine.World
         public const int LocalPlayers = 0x38; //Engine.GameInstance
         public const int PlayerController = 0x30; //Engine.Player
-        public const int PlayerCameraManager = 0x350; //Engine.PlayerController
+        public const int PlayerCameraManager = 0x360; //Engine.PlayerController
                                                       //public const int PlayerMinimalViewInfo = 0x22B0;
-        public const int PlayerMinimalViewInfo = 0x1320; //Engine.PlayerCameraManager CameraCachePrivate
-        public const int PlayerHUD = 0x348; //Engine.PlayerController
+        public const int PlayerMinimalViewInfo = 0x1410; //Engine.PlayerCameraManager CameraCachePrivate
+        public const int PlayerHUD = 0x358; //Engine.PlayerController
         public const int NameIndex = 0x18; //uObject
         public const int ComponentToWorld = 0x240;
-        public const int Instigator = 0x188; //Engine.Actor
+        public const int Instigator = 0x1a0; //Engine.Actor
                                              //public const int isOpen = 0x3F0;
                                              //public const int isHidden = 0x40C;
         [StructLayout(LayoutKind.Explicit)]
         public struct ActorStatus
         {
             [FieldOffset(0x3F8)]
-            public bool IsOpen; // This will start at the base address + 0x3F0
+            public bool IsOpen; // This will start at the base address + 0x3F0 // didnt update, not sure what this is, but it could be 0xd50 which is DCactorstatus
         }
 
         public class PlayerState
         {
-            public const int PlayerPawnPrivate = 0x310; //Engine.PlayerState
-            public const int PlayerNamePrivate = 0x330; //Engine.PlayerState
+            public const int PlayerPawnPrivate = 0x320; //Engine.PlayerState
+            public const int PlayerNamePrivate = 0x340; //Engine.PlayerState
         }
         public class PawnPrivate
         {
-            public const int SkeletalMeshComponent = 0x320;
-            public const int ComponentToWorld = 0x240;
-            public const int AccountID = 0x7D0; //fString  DungeonCrawler.DCCharacterBase
-            public const int NickNameCached = 0x858; //fNickname
-            public const int MeshDeformerInstance = 0x5F0; //Engine.SkinnedMeshComponent
-            public const int RootComponent = 0x1A0; //Engine.Actor
-            public const int PlayerController = 0x2D0;
-            public const int AbilitySystemComponent = 0x730; //DungeonCrawler.DCCharacterBase
-            public const int AccountDataReplication = 0x7F0; //DungeonCrawler.DCCharacterBase
-            public const int EquipmentInventory = 0xFAB;
-            public const int SpawnedAttributes = 0x10A0;
+            public const int SkeletalMeshComponent = 0x408; //unable to find
+            public const int ComponentToWorld = 0x188;
+            public const int AccountID = 0x7d8; //fString  DungeonCrawler.DCCharacterBase
+            public const int NickNameCached = 0x418; //fNickname //unable to find this, put my best guess, not sure what this is used for either.
+            public const int MeshDeformerInstance = 0x568; //Engine.SkinnedMeshComponent
+            public const int RootComponent = 0x1b8; //Engine.Actor
+            public const int PlayerController = 0x2d8;
+            public const int AbilitySystemComponent = 0x708; //DungeonCrawler.DCCharacterBase
+            public const int AccountDataReplication = 0x7f8; //DungeonCrawler.DCCharacterBase
+            public const int EquipmentInventory = 0xa68; //was also unable to find this, used my best guess. 
+            public const int SpawnedAttributes = 0x1088;
         }
         public class RootComponent
         {
